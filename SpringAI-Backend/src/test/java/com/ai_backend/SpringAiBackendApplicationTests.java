00185e93f936a61659c4cd10e4f4dcf63f905f30
Anil Dadasaheb Poul <118977274+Anilpoul@users.noreply.github.com>
2025-01-14 20:13:28 +0530

    Created AI Chat application by using Ollama.
    Created AI chat Application using Spring AI where We have used Ollama model which is free to use

---
+++ SpringAI-Backend/src/test/java/com/ai_backend/SpringAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
